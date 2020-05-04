3b48173f14e6095e74ba338d2139355da3dd7c2b
jan <jan.mastalier@gmail.com>
2020-05-04 16:25:35 +0200

    Update Post (not working)

--- src/app/post/post-create.service.ts
+++ src/app/post/post-create.service.ts
@@ -12,6 +12,12 @@ export class PostCreateService {
   constructor(private http: HttpClient) { }
 
   createPost(posts: Posts[]) {
+
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: 'Basic dXNlcjpQNWlVVFhhdmZCSnU='
+    });
+
     return this.http.post(`http://34.65.223.230/:9200/users/_doc`, posts).
     pipe(
       catchError( error => {
